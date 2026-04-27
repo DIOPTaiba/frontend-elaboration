d4366d010a71998a1421a83298533f3bf8f7bb0d
Dinfo <dinfo@192.168.1.23>
2026-04-27 01:24:34 +0000

    maj Emplois Effectif Section

--- src/app/pages/pppb/depensesPersonnelles-emplois/maj-emplois-effectif-section/maj-emplois-effectif-section.component.ts
+++ src/app/pages/pppb/depensesPersonnelles-emplois/maj-emplois-effectif-section/maj-emplois-effectif-section.component.ts
@@ -1,12 +1,408 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { AppCodeViewComponent } from 'src/app/components/code-view/code-view.component';
+
+// snippets
+import { EXPAND_TABLE_HTML_SNIPPET } from 'src/app/pages/tables/expand-table/code/expand-table-html-snippet';
+import { EXPAND_TABLE_TS_SNIPPET } from 'src/app/pages/tables/expand-table/code/expand-table-ts-snippet';
+
+import { Highlight, HighlightAuto } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { FormsModule } from '@angular/forms';
+
+const CHAPITRE_DATA: objetChapitre[] = [
+  {
+    code: '25025330',
+    libelle: 'Direction de la programmation budgétaire',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    code: '25025331',
+    libelle: 'Direction de la Solde',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    code: '25025332',
+    libelle: 'Direction des Systèmes d’information',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    code: '25025333',
+    libelle: 'Direction du Contrôle budgétaire',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    code: '2502533',
+    libelle: 'Direction des Pensions',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+];
+const AGENT_DATA: objetAgent[] = [
+  {
+    chapitre: 'Direction SI DGB',
+    emploi: 'Ing. Informaticien',
+    matricule: '12345678',
+    nom: 'xxxxx yyy',
+    age: 0,
+    statut: 'agent',
+    action: 'action',
+    activite: 'activité',
+    effectifs: 25,
+    budgetPrevu: 25000000,
+  },
+  {
+    chapitre: 'Direction SI DGB',
+    emploi: 'Ing. Informaticien',
+    matricule: '12345678',
+    nom: 'xxxxx yyy',
+    age: 0,
+    statut: 'agent',
+    action: 'action',
+    activite: 'activité',
+    effectifs: 25,
+    budgetPrevu: 25000000,
+  },
+  {
+    chapitre: 'Direction SI DGB',
+    emploi: 'Ing. Informaticien',
+    matricule: '12345678',
+    nom: 'xxxxx yyy',
+    age: 0,
+    statut: 'agent',
+    action: 'action',
+    activite: 'activité',
+    effectifs: 25,
+    budgetPrevu: 25000000,
+  },
+  {
+    chapitre: 'Direction SI DGB',
+    emploi: 'Ing. Informaticien',
+    matricule: '12345678',
+    nom: 'xxxxx yyy',
+    age: 0,
+    statut: 'agent',
+    action: 'action',
+    activite: 'activité',
+    effectifs: 25,
+    budgetPrevu: 25000000,
+  },
+  {
+    chapitre: 'Direction SI DGB',
+    emploi: 'Ing. Informaticien',
+    matricule: '12345678',
+    nom: 'xxxxx yyy',
+    age: 0,
+    statut: 'agent',
+    action: 'action',
+    activite: 'activité', 
+    effectifs: 25,
+    budgetPrevu: 25000000,
+  },
+];
+const EMPLOI_DATA: objetType[] = [
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Emp01',
+    libelle: 'Ingénieurs informaticiens',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Emp02',
+    libelle: 'Ingénieurs informaticiens',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Emp03',
+    libelle: 'Ingénieurs informaticiens',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Emp04',
+    libelle: 'Ingénieurs informaticiens',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Emp05',
+    libelle: 'Ingénieurs informaticiens',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+];
+const ACTION_DATA: objetType[] = [
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Action01',
+    libelle: 'Élaboration des lois de finances',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Action02',
+    libelle: 'Élaboration des lois de finances',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Action03',
+    libelle: 'Élaboration des lois de finances',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Action04',
+    libelle: 'Élaboration des lois de finances',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Action05',
+    libelle: 'Élaboration des lois de finances',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  }
+];
+
+const ACTIVITE_DATA: objetType[] = [
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Activite01',
+    libelle: 'Appui informatique',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Activite02',
+    libelle: 'Appui informatique',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Activite03',
+    libelle: 'Appui informatique',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Activite04',
+    libelle: 'Appui informatique',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  },
+  {
+    action: 'Élaboration des lois de finances',
+    code: 'Activite05',
+    libelle: 'Appui informatique',
+    effectifs0: 100,
+    agentsSolde: 100,
+    contractuels: 100,
+    total: 100,
+    ecart: 100,
+    dotation: 100
+  }
+];
 
 @Component({
   selector: 'app-maj-emplois-effectif-section',
-  imports: [MaterialModule],
+  imports: [
+    MaterialModule,
+    MatCardModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    CommonModule,
+    MatDividerModule,
+    FormsModule,
+    TablerIconsModule,
+  ],
   templateUrl: './maj-emplois-effectif-section.component.html',
   styleUrl: './maj-emplois-effectif-section.component.scss',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
-export class MajEmploisEffectifSectionComponent {
+export class MajEmploisEffectifSectionComponent implements OnInit {
 
+  choix: number = 1; // valeur par défaut
+
+
+  listeChapitre = CHAPITRE_DATA;
+  listeAgent = AGENT_DATA;
+  listeEmplois = EMPLOI_DATA;
+  listeActions = ACTION_DATA;
+  listeActivites = ACTIVITE_DATA;
+
+  columnsChapitreToDisplay = ['code', 'libelle', 'effectifs0', 'agentsSolde', 'contractuels', 'total', 'ecart', 'dotation'];
+  columnsEmploiToDisplay = ['code', 'libelle', 'effectifs0', 'agentsSolde', 'contractuels', 'total', 'ecart', 'dotation'];
+  columnsActionToDisplay = ['code', 'libelle', 'effectifs0', 'agentsSolde', 'contractuels', 'total', 'ecart', 'dotation'];
+  columnsActiviteToDisplay = ['action', 'code', 'libelle', 'effectifs0', 'agentsSolde', 'contractuels', 'total', 'ecart', 'dotation'];
+
+  
+
+  columnsChapitreToDisplayWithExpand = [...this.columnsChapitreToDisplay, 'expand'];
+  columnsEmploiToDisplayWithExpand = [...this.columnsEmploiToDisplay, 'expand'];
+  columnsActionToDisplayWithExpand = [...this.columnsActionToDisplay, 'expand'];
+  columnsActiviteToDisplayWithExpand = [...this.columnsActiviteToDisplay, 'expand'];
+
+  expandedElementChapitre: objetChapitre | null = null;
+  expandedElementEmploi: objetType | null = null;
+  expandedElementAction: objetType | null = null;
+  expandedElementActivite: objetType | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
+
+export interface objetChapitre {
+  code: string;
+  libelle: string;
+  effectifs0: number;
+  agentsSolde: number;
+  contractuels: number;
+  total: number;
+  ecart: number;
+  dotation: number;
+}
+
+export interface objetAgent {
+  chapitre: string;
+  emploi: string;
+  matricule: string;
+  nom: string;
+  age: number;
+  statut: string;
+  action: string;
+  activite: string;
+  effectifs: number;
+  budgetPrevu: number;
+}
+
+export interface objetType {
+  action: string;
+  code: string;
+  libelle: string;
+  effectifs0: number;
+  agentsSolde: number;
+  contractuels: number;
+  total: number;
+  ecart: number;
+  dotation: number;
 }
